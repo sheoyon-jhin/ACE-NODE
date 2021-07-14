3cbb6ec40848423a4b17c2b49d6cad11836fd619
JhinSheoYon <sheoyonj@naver.com>
2021-07-14 04:56:58 +0000

    latentode code revised

--- Physionet/test.sh
+++ Physionet/test.sh
@@ -2,3 +2,5 @@ for seed in 1991 2019 2020 2021 2022
 do
     CUDA_VISIBLE_DEVICES=0 python3 run_models.py --random-seed $seed --niters 20 -n 8000 -l 20 --dataset physionet --attentive-ode --rec-dims 40 --rec-layers 3 --gen-layers 3 --units 50 --gru-units 50 --quantization 0.016 --classif >/home/bigdyl/latent_ode/test2.csv
 done
+
+#CUDA_VISIBLE_DEVICES=1 python3 run_models.py --random-seed 2021 --niters 100 -n 8000 -l 20 --dataset physionet --attentive-ode --rec-dims 40 --rec-layers 3 --gen-layers 3 --units 50 --gru-units 50 --quantization 0.016 --classif 
